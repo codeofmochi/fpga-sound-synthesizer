08ffd426fc8c30437355f36463fd8ea301436698
Alexandre Chau <accounts@chau.moe>
2020-06-10 17:31:07 +0200

    Compile system

--- src/sw/nios/fpga_sound_synthesizer_bsp/system.h
+++ src/sw/nios/fpga_sound_synthesizer_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'system'
  * SOPC Builder design path: /home/vm/Documents/rtes/Lab4/src/hw/system.sopcinfo
  *
- * Generated: Wed Jun 10 13:48:04 CEST 2020
+ * Generated: Wed Jun 10 17:27:20 CEST 2020
  */
 
 /*
@@ -293,7 +293,7 @@
 #define SYSID_QSYS_0_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_0_NAME "/dev/sysid_qsys_0"
 #define SYSID_QSYS_0_SPAN 8
-#define SYSID_QSYS_0_TIMESTAMP 1591789297
+#define SYSID_QSYS_0_TIMESTAMP 1591802379
 #define SYSID_QSYS_0_TYPE "altera_avalon_sysid_qsys"
 
 #endif /* __SYSTEM_H_ */
